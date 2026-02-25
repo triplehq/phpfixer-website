bf5c714e6d44de05b33d5cd4a664808158edb319
Tamas Erdelyi <support@terdelyi.co.uk>
2026-02-25 15:29:26 +0000

    Fix urls

--- content/config.php
+++ content/config.php
@@ -2,10 +2,7 @@
 
 use Terdelyi\Phanstatic\Models\Config;
 
-// For local development, leave empty for relative paths
-$baseUrl = getenv('DEPLOY_PRIME_URL') ?: getenv('URL') ?: '';
-
 return new Config(
-    baseUrl: $baseUrl,
+    baseUrl: getenv('URL') ?? '',
     title:'PHPFixer'
 );
